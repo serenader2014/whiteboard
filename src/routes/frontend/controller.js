70cdb1516581ddd313d007dc166a24342bbd9f3e
serenader <xyslive@gmail.com>
2017-04-26 18:50:06 +0800

    [client] Update: index

--- src/routes/frontend/controller.js
+++ src/routes/frontend/controller.js
@@ -1,4 +1,4 @@
-import { setting } from '../../api'
+import { setting, post } from '../../api'
 
 export async function homePage(ctx) {
   const settings = await setting.listAll({ context: 'internal' }, { type: 'blog' })
@@ -6,7 +6,14 @@ export async function homePage(ctx) {
     obj[item.key] = item.value
     return obj
   }, {})
+  const posts = await post.listPublishedPosts({ context: 'internal' }, { pageSize: blogSettings.post_per_page })
   await ctx.render('index.hbs', {
-    settings: blogSettings
+    settings: blogSettings,
+    posts: posts.data,
+    pagination: {
+      page: posts.meta.page,
+      totalPage: posts.meta.pageCount,
+      totalPosts: posts.meta.rowCount
+    }
   })
 }
